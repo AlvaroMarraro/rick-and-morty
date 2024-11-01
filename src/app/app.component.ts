9e2008c7a8f3938333982ea291fe25ed6de0fa4d
Alvaro Marraro <alvaromarraro@gmail.com>
2024-11-01 00:51:46 -0300

    Finalizado

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,13 +11,11 @@ export class AppComponent {
   scrollToTop() {
     window.scrollTo({ top: 0, behavior: 'smooth' });
   }
-
-  // Detectar el desplazamiento
   @HostListener('window:scroll', [])
   onWindowScroll() {
     const button = document.getElementById('backToTop');
     if (button) {
-      if (window.pageYOffset > 100) { // Mostrar el botón si se ha desplazado más de 100px
+      if (window.pageYOffset > 100) {
         button.style.display = 'block';
       } else {
         button.style.display = 'none';
